90cd9e3e6e59dc30fb6f3b215e1a5b1228c3ad9e
jinju2828 <jinju2828@gmail.com>
2023-01-01 16:58:14 -0800

    685 with binary search

---
+++ jewelsAndStones(2)-771.js
@@ -0,0 +1,39 @@
+
+//solution 1 이게 젤 빠름, set, reduce
+var numJewelsInStones = function(J, S) {
+    const jewels = new Set(J);
+    console.log(jewels);
+    console.log(100+jewels.has(S[0]));
+    return [...S].reduce((res, s) => res + jewels.has(s), 0); //0은 accumulator로 initial value. 숫자로는 0, object면 {} empty로
+}
+
+// solution 2 두번째로 빠름. filter! truthy 한 결과만 array로 return
+var numJewelsInStones = (J, S) => S.split('').filter(elem => J.includes(elem)).length;
+// 혹은 [...S].filter((char) => J.indexOf(char) > -1 ).length // S.split('')랑 [...S]랑 같으므로
+
+//solution 3 어쨌든 for loop에다가 indexOf 돌리는 거라 내 거랑 time complexity 비슷함
+var numJewelsInStones = function(J, S) {
+    let num = 0;
+    for(var i=0; i<S.length; i++){
+        if(J.indexOf(S[i])>-1){
+            num++
+        }
+    }
+    return num;
+};
+
+//이것도 빠른 솔루션
+var numJewelsInStones = function(J, S) {
+    if (!J || !S) return 0;
+
+    let count = 0;
+    for (const c of S) {
+        console.log(c);
+        if (J.includes(c)) count++;
+    }
+
+    return count;
+};
+
+var res = numJewelsInStones('aAAA', 'aAAbbb');
+console.log(res);
---
+++ numberOfIslands-200.js
@@ -0,0 +1,69 @@
+//What is Depth First Search?
+
+// var DFS = function (node){
+//     let s = new Stack(this.nodes.length);
+//     let explored = new Set();
+//     s.push(node);
+//
+//     explored.add(node);
+//
+//     while(!s.isEmpty()){
+//         let t = s.pop();
+//
+//         console.log(t);
+//
+//         this.edges[t]
+//             .filter(n=>!explored.has(n))
+//             .forEach(n=> {
+//                 explored.add(n);
+//                 s.push(n);
+//             })
+//     }
+// }
+//
+// let g = new Graph(7);
+// g.addNode("A");
+// g.addNode("B");
+// g.addNode("C");
+// g.addNode("D");
+// g.addNode("E");
+// g.addNode("F");
+// g.addNode("G");
+//
+// g.addEdge("A", "C");
+// g.addEdge("A", "B");
+// g.addEdge("A", "D");
+// g.addEdge("D", "E");
+// g.addEdge("E", "F");
+// g.addEdge("B", "G");
+//
+// g.DFS("A");
+
+//DFS solution stack - LIFO 자식으로 계속 가다가 형제노드
+function numIslands(grid) {
+    const H = grid.length;
+    const W = H && grid[0].length;
+    let count = 0;
+
+    for (let r = 0; r < H; r++) {
+        for (let c = 0; c < W; c++) {
+            if (grid[r][c] === '0') continue; // count 를 늘리지 않는다. 방문 했던 곳을 0으로 하면 DFS를 trigger하는 node로 간주되지 않으므로!
+            count++;
+            dfs(r, c);
+        }
+    }
+    return count;
+    function dfs(r,c){
+        if (r<0 || c<0 || r===H || c===W) return; // grid 밖으로 넘어가면 terminate
+        if (grid[r][c] ==='0') return; // island 가다가 water 0을 마주치면 terminate
+
+        grid[r][c] = '0'; // mark as 0 when visited
+        dfs(r-1, c);
+        dfs(r+1, c);
+        dfs(r, c-1);
+        dfs(r, c+1);
+
+    }
+}
+
+
