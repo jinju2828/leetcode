f2acddbd200cef4dc905cf52862edb2c9cc0c097
jinju2828 <jinju2828@gmail.com>
2023-01-03 13:07:07 -0800

    150 reverse polish notation using stack

---
+++ reversePolishNotatoin-150.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+function evalRPN(token) {
+    let res = 0, stack = new Array();
+    for(i of token) {
+        if(token.length === 1) return token[0];
+        if(!isNaN(i)) {
+            stack.push(parseInt(i));
+        }else if(i === '+') {
+            res = 0;
+            res += stack.pop();
+            res += stack.pop();
+            stack.push(res);
+        }else if(i === '-') {
+            res = stack.at(-2) - stack.at(-1);
+            stack.pop();
+            stack.pop();
+            stack.push(res);
+        }else if(i === '*') {
+            res = 1;
+            res *= stack.pop();
+            res *= stack.pop();
+            stack.push(res);
+        }else if(i === '/') {
+            res = Math.trunc(stack.at(-2) / stack.at(-1));
+            stack.pop();
+            stack.pop();
+            stack.push(res);
+        }
+        console.log(stack);
+    }
+    return res;
+}
