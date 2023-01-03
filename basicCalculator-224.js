d0262e36464a2db55267a56b63a0ada3d1975697
jinju2828 <jinju2828@gmail.com>
2023-01-02 20:32:29 -0800

    224 basic calculator i with stack

---
+++ basicCalculator-224.js
@@ -0,0 +1,57 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+function calculate(s) {
+    let trimmed = s.split(' ').join('');
+
+    let num = 0,
+        result = 0,
+        sign = 1,
+        stack = new Array();
+
+    for(let i = 0; i < trimmed.length; i++) {
+        if(!isNaN(trimmed[i])) {
+            num = (num * 10) + (trimmed[i] - '0');
+            // num = (num * 10) + parseInt(trimmed[i]));
+            console.log(num);
+        }else if(trimmed[i] === '+') {
+            result += num * sign;
+            console.log(result, num, sign);
+            sign = 1;
+            num = 0;
+        }else if(trimmed[i] === '-') {
+            result += num * sign;
+            console.log(result, num, sign);
+            sign = -1;
+            num = 0;
+            console.log("after", result, num, sign);
+        }else if(trimmed[i] === '(') {
+            stack.push(result);
+            stack.push(sign);
+            console.log(stack);
+            result = 0;
+            sign = 1;
+            num = 0;
+        }else if(trimmed[i] === ')') {
+            result += num * sign;
+            console.log("result", result);
+            // result *= stack.slice(-1)[0];
+            // stack.pop();
+            result *= stack.pop();
+            console.log("stack1", stack);
+            console.log("result1", result);
+            // result += stack.slice(-1)[0];
+            // stack.pop();
+            result += stack.pop();
+            console.log("stack2", stack);
+             console.log("result2", result);
+            num = 0;
+            sign = 1;
+        }
+    }
+
+    result += num * sign;
+
+    return result;
+}
