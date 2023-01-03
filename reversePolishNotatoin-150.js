e01091cf35a6728f623b988231a8f8718334d79f
jinju2828 <jinju2828@gmail.com>
2023-01-03 13:41:56 -0800

    150 make simple stack.pop()

--- reversePolishNotatoin-150.js
+++ reversePolishNotatoin-150.js
@@ -5,32 +5,41 @@
 function evalRPN(token) {
     let res = 0, stack = new Array();
     for(i of token) {
-        if(token.length === 1) return token[0];
+        // if(token.length === 1) return token[0];
         if(!isNaN(i)) {
             stack.push(parseInt(i));
         }else if(i === '+') {
             res = 0;
-            res += stack.pop();
-            res += stack.pop();
+            // res += stack.pop();
+            // res += stack.pop();
+            // stack.push(res);
+            res = stack.pop() + stack.pop();
             stack.push(res);
         }else if(i === '-') {
-            res = stack.at(-2) - stack.at(-1);
-            stack.pop();
-            stack.pop();
+            // res = stack.at(-2) - stack.at(-1);
+            // stack.pop();
+            // stack.pop();
+            // stack.push(res);
+            let a = stack.pop(), b = stack.pop();
+            res = b - a;
             stack.push(res);
         }else if(i === '*') {
             res = 1;
-            res *= stack.pop();
-            res *= stack.pop();
+            // res *= stack.pop();
+            // res *= stack.pop();
+            // stack.push(res);
+            res = stack.pop() * stack.pop();
             stack.push(res);
         }else if(i === '/') {
-            res = Math.trunc(stack.at(-2) / stack.at(-1));
-            stack.pop();
-            stack.pop();
+            // res = Math.trunc(stack.at(-2) / stack.at(-1));
+            // stack.pop();
+            // stack.pop();
+            // stack.push(res);
+            let a = stack.pop(), b = stack.pop();
+            res = Math.trunc(b / a);
             stack.push(res);
         }
         console.log(stack);
     }
-    // return res;
     return stack[0];
 }
