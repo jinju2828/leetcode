cb2e52f6d60de9d65140c8178d0c50cc25c1ec48
jinju2828 <jinju2828@gmail.com>
2023-01-02 20:34:01 -0800

    224 basic calculator i comment out console log

--- basicCalculator-224.js
+++ basicCalculator-224.js
@@ -14,38 +14,38 @@ function calculate(s) {
         if(!isNaN(trimmed[i])) {
             num = (num * 10) + (trimmed[i] - '0');
             // num = (num * 10) + parseInt(trimmed[i]));
-            console.log(num);
+            // console.log(num);
         }else if(trimmed[i] === '+') {
             result += num * sign;
-            console.log(result, num, sign);
+            // console.log(result, num, sign);
             sign = 1;
             num = 0;
         }else if(trimmed[i] === '-') {
             result += num * sign;
-            console.log(result, num, sign);
+            // console.log(result, num, sign);
             sign = -1;
             num = 0;
-            console.log("after", result, num, sign);
+            // console.log("after", result, num, sign);
         }else if(trimmed[i] === '(') {
             stack.push(result);
             stack.push(sign);
-            console.log(stack);
+            // console.log(stack);
             result = 0;
             sign = 1;
             num = 0;
         }else if(trimmed[i] === ')') {
             result += num * sign;
-            console.log("result", result);
+            // console.log("result", result);
             // result *= stack.slice(-1)[0];
             // stack.pop();
             result *= stack.pop();
-            console.log("stack1", stack);
-            console.log("result1", result);
+            // console.log("stack1", stack);
+            // console.log("result1", result);
             // result += stack.slice(-1)[0];
             // stack.pop();
             result += stack.pop();
-            console.log("stack2", stack);
-             console.log("result2", result);
+            // console.log("stack2", stack);
+            //  console.log("result2", result);
             num = 0;
             sign = 1;
         }
