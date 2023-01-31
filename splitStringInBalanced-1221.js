92dd891b2730f4df54dc56d8c1d8fef2c88473c8
jinju2828 <jinju2828@gmail.com>
2023-01-31 13:25:55 -0800

    1221 split a string in balanced strings - no need to reset counter

--- splitStringInBalanced-1221.js
+++ splitStringInBalanced-1221.js
@@ -3,18 +3,18 @@
  * @return {number}
  */
 var balancedStringSplit = function(s) {
-    let stack = [];
+    // let stack = [];
     let res = 0;
     let r = 0, l = 0;
     for(i of s){
-        stack.push(i);
+        // stack.push(i);
         if(i==='R') r++;
         if(i==='L') l++;
         if(r===l) {
-            stack = [];
+            // stack = [];
             res++;
-            r = 0;
-            l = 0;
+            // r = 0;
+            // l = 0;
         }
     }
     return res;
