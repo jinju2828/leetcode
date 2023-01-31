1c2f199a5f5cf9967d7ad74afede381f2c3118cb
jinju2828 <jinju2828@gmail.com>
2023-01-31 13:27:32 -0800

    1221 split a string in balanced strings - no need to reset counter python

---
+++ splitStringInBalance-1221.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def balancedStringSplit(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        w_count = l_count = r_count = 0
+        for ch in s:
+            if ch == "L":
+                l_count += 1
+            else:
+                r_count += 1
+            if l_count == r_count:
+                w_count += 1
+        return w_count
