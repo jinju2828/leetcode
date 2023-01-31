559bb7a5fb3032ca697efc0b7838abf467d7506c
jinju2828 <jinju2828@gmail.com>
2023-01-31 11:37:16 -0800

    1221 split a string in balanced strings

---
+++ splitStringInBalanced-1221.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+    let stack = [];
+    let res = 0;
+    let r = 0, l = 0;
+    for(i of s){
+        stack.push(i);
+        if(i==='R') r++;
+        if(i==='L') l++;
+        if(r===l) {
+            stack = [];
+            res++;
+            r = 0;
+            l = 0;
+        }
+    }
+    return res;
+
+};
