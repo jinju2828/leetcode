fcd4639155c7994aa4a7d618f2187c8c117c3a0c
jinju2828 <jinju2828@gmail.com>
2023-02-09 13:56:59 -0800

    1470. Shuffle the Array

---
+++ shuffleArray-1470.ts
@@ -0,0 +1,9 @@
+function shuffle(nums: number[], n: number): number[] {
+    let ans:number[] = [];
+    for(let i = 0; i < nums.length/2; i++){
+        console.log(nums[i])
+        ans.push(nums[i]);
+        ans.push(nums[i+n]);
+    }
+    return ans;
+};
