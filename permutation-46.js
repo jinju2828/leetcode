37f707e248b9e3f9cba737e50cb196a5a0b7668b
Pearl Fall <jinju2828@gmail.com>
2023-03-01 22:08:22 -0800

    Create permutation-46.js

---
+++ permutation-46.js
@@ -0,0 +1,18 @@
+let permute = function (nums, arr = [], res = []) {
+    //base case
+    if(nums.length === 0) res.push([...arr]);
+
+    for(let i = 0; i < nums.length; i++) {
+        let rest = nums.filter((num, index) => index !== i);
+        arr.push(nums[i]);
+        console.log("arr1", arr);
+        console.log("nums1", nums);
+        console.log("rest1", rest);
+        permute(rest, arr, res);
+        arr.pop();
+        console.log("arr2", arr);
+        console.log("nums2", nums);
+    }
+
+    return res;
+}
